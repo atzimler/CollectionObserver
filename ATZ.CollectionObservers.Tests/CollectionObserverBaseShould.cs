15e0aa7e862b79a60eaa34d4716ab0848cf0e634
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-05-05 03:02:29 +1000

    Adding test cases.

---
+++ ATZ.CollectionObservers.Tests/CollectionObserverBaseShould.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Collections.ObjectModel;
+
+namespace ATZ.CollectionObservers.Tests
+{
+    [TestFixture]
+    public class CollectionObserverBaseShould
+    {
+        [Test]
+        public void AllowAddingToSourceAndTargetAtTheSameTime()
+        {
+            var sourceCollection = new ObservableCollection<int>();
+            var targetCollection = new ObservableCollection<int>();
+
+            var collectionObserver = new TestCollectionObserver
+            {
+                Source = sourceCollection,
+                Target = targetCollection
+            };
+
+            collectionObserver.Add(13, 42);
+
+            Assert.AreEqual(1, sourceCollection.Count);
+            Assert.AreEqual(1, targetCollection.Count);
+            Assert.AreEqual(13, sourceCollection[0]);
+            Assert.AreEqual(42, targetCollection[0]);
+        }
+
+    }
+}
---
+++ ATZ.CollectionObservers.Tests/TestCollectionObserver.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ATZ.CollectionObservers.Tests
+{
+    public class TestCollectionObserver : CollectionObserverBase<int, int, ObservableCollection<int>>
+    {
+        public ObservableCollection<int> Source
+        {
+            get => SourceCollection;
+            set => SourceCollection = value;
+        }
+
+        public ObservableCollection<int> Target
+        {
+            get => TargetCollection;
+            set => TargetCollection = value;
+        }
+
+        public override void ClearCollection()
+        {
+            TargetCollection?.Clear();
+        }
+
+        public override void AddItem(int item)
+        {
+            TargetCollection?.Add(item);
+        }
+
+        public override int CreateItem(int sourceItem)
+        {
+            return sourceItem;
+        }
+
+        public override void InsertItem(int index, int item)
+        {
+            TargetCollection?.Insert(index, item);
+        }
+
+        public override void MoveItem(int oldIndex, int newIndex)
+        {
+            TargetCollection?.Move(oldIndex, newIndex);
+        }
+
+        public override void RemoveItem(int index)
+        {
+            TargetCollection?.RemoveAt(index);
+        }
+
+        public override void ReplaceItem(int index, int newItem)
+        {
+            var collection = TargetCollection;
+            if (collection == null)
+            {
+                return;
+            }
+
+            collection[index] = newItem;
+        }
+    }
+}

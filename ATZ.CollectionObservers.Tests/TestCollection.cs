bd4fbeb12ed121d947bf9cf29ac7e07c40755064
Attila Tamas Zimler <attila.tamas.zimler@icloud.com>
2017-05-05 08:42:27 +1000

    Increasing test coverage to correct level.

---
+++ ATZ.CollectionObservers.Tests/TestCollection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace ATZ.CollectionObservers.Tests
+{
+    public class TestCollection : ObservableCollection<int>
+    {
+        public bool AddCalled { get; set; }
+        public bool RemoveCalled { get; set; }
+
+        public override event NotifyCollectionChangedEventHandler CollectionChanged
+        {
+            add
+            {
+                AddCalled = true;
+                base.CollectionChanged += value;
+            }
+            remove
+            {
+                RemoveCalled = true;
+                base.CollectionChanged -= value;
+            }
+        }
+    }
+}
